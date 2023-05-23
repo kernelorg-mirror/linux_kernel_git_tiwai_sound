8cc99a7df1430b8b97a46068fd3d6fb3f1ba65ad
Takashi Iwai <tiwai@suse.de>
2023-05-23 09:53:28 +0200

    ALSA: usb-audio: Manage number of rawmidis globally
    We're going to create rawmidi objects for MIDI 2.0 in a different code
    from the current code for USB-MIDI 1.0.  As a preliminary work, this
    patch adds the number of rawmidi objects to keep globally in a
    USB-audio card instance, so that it can be referred from both MIDI 1.0
    and 2.0 code.
    
    Reviewed-by: Jaroslav Kysela <perex@perex.cz>
    Link: https://lore.kernel.org/r/20230523075358.9672-8-tiwai@suse.de
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit bb1bf4fa5953418c131796ee745c59899d34a149)

--- sound/usb/usbaudio.h
+++ sound/usb/usbaudio.h
@@ -48,6 +48,7 @@ struct snd_usb_audio {
 	struct list_head iface_ref_list; /* list of interface refcounts */
 	int pcm_devs;
 
+	unsigned int num_rawmidis;	/* number of created rawmidi devices */
 	struct list_head midi_list;	/* list of midi interfaces */
 
 	struct list_head mixer_list;	/* list of mixer interfaces */
